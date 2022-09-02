3a23ef8165dc5b381f63ff2e65743640c5984d22
unknown <aguustinn18@gmail.com>
2022-09-02 02:49:05 -0300

    actualizacion 3

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -17,20 +17,16 @@ const ItemDetail = () => {
   console.log(data);
   return (
     <div className="item-container">
-      <h1>Container detalles</h1>
+      <h1>Detalles del producto</h1>
       {loading ? (
         <h2>Loading...</h2>
       ) : (
         <div className="item-detail">
           <img src={data.img} alt="" />
           <h2>{data.nombre}</h2>
-          <h3>{data.precio}</h3>
-
-          <div className="detail">
-            <h4>{data.nombre}</h4>
-            <p>{data.precio}</p>
-          </div>
-          <p>{data.precio}</p>
+          <h3>${data.precio}</h3>
+          <h5>Descripción del producto: {data.descripcion}</h5>
+          <button className="btn btn-primary">Comprar</button>
         </div>
       )}
     </div>
--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,77 +1,77 @@
-import nike_1 from '../img/Nike/Zapatillas_Nike_Air_Force_1LV8.jpg'
-import nike_2 from '../img/Nike/Zapatillas_Nike_Air_Max_Dawn.jpg'
-import nike_3 from '../img/Nike/Zapatillas_Nike_Air_Max_Dia.jpg'
-import adidas_1 from '../img/Adidas/Zapatillas_adidas_Continental_80.jpg'
-import adidas_2 from '../img/Adidas/Zapatillas_adidas_Forum_Mid.jpg'
-import adidas_3 from '../img/Adidas/Zapatillas_adidas_Ozelia.jpg'
-import puma_1 from '../img/Puma/Zapatillas_Puma_Mayze.jpg'
-import puma_2 from '../img/Puma/Zapatillas_Puma_Mirage.jpg'
-import puma_3 from '../img/Puma/Zapatillas_Puma_RS.jpg'
+import nike_1 from "../img/Nike/Zapatillas_Nike_Air_Force_1LV8.jpg";
+import nike_2 from "../img/Nike/Zapatillas_Nike_Air_Max_Dawn.jpg";
+import nike_3 from "../img/Nike/Zapatillas_Nike_Air_Max_Dia.jpg";
+import adidas_1 from "../img/Adidas/Zapatillas_adidas_Continental_80.jpg";
+import adidas_2 from "../img/Adidas/Zapatillas_adidas_Forum_Mid.jpg";
+import adidas_3 from "../img/Adidas/Zapatillas_adidas_Ozelia.jpg";
+import puma_1 from "../img/Puma/Zapatillas_Puma_Mayze.jpg";
+import puma_2 from "../img/Puma/Zapatillas_Puma_Mirage.jpg";
+import puma_3 from "../img/Puma/Zapatillas_Puma_RS.jpg";
 
 const productos = [
-    {
-        id: 1,
-        nombre: 'Nike_Air_Force_1LV8',
-        img: [nike_1],
-        precio: 30000,
-    },
-    {
-        id: 2,
-        nombre: 'Zapatillas_Nike_Air_Max_Dawn',
-        img: [nike_2],
-        precio: 20000,
-    },
-    {
-        id: 3,
-        nombre: 'Zapatillas_Nike_Air_Max_Dia',
-        img: [nike_3],
-        precio: 15000,
-    },
-    {
-        id: 4,
-        nombre: 'adidas_Continental_80',
-        img: [adidas_1],
-        precio: 15000,
-    },
-    {
-        id: 5,
-        nombre: 'Zapatillas_adidas_Forum_Mid',
-        img: [adidas_2],
-        precio: 20000,
-    },
-    {
-        id: 6,
-        nombre: 'Zapatillas_adidas_Ozelia',
-        img: [adidas_3],
-        precio: 35000,
-    },
-    {
-        id: 7,
-        nombre: 'Zapatillas_Puma_Mayze',
-        img: [puma_1],
-        precio: 35000,
-    },
-    {
-        id: 8,
-        nombre: 'Zapatillas_Puma_Mirage',
-        img: [puma_2],
-        precio: 15000,
-    },
-    {
-        id: 9,
-        nombre: 'Zapatillas_Puma_RS',
-        img: [puma_3],
-        precio: 25000,
-    },
-]
+  {
+    id: 1,
+    nombre: "Nike Air Force 1LV8",
+    img: [nike_1],
+    precio: 30000,
+    descripcion:
+      "loremloremloremloremloremlorem",
+  },
+  {
+    id: 2,
+    nombre: "Zapatillas_Nike_Air_Max_Dawn",
+    img: [nike_2],
+    precio: 20000,
+  },
+  {
+    id: 3,
+    nombre: "Zapatillas_Nike_Air_Max_Dia",
+    img: [nike_3],
+    precio: 15000,
+  },
+  {
+    id: 4,
+    nombre: "adidas_Continental_80",
+    img: [adidas_1],
+    precio: 15000,
+  },
+  {
+    id: 5,
+    nombre: "Zapatillas_adidas_Forum_Mid",
+    img: [adidas_2],
+    precio: 20000,
+  },
+  {
+    id: 6,
+    nombre: "Zapatillas_adidas_Ozelia",
+    img: [adidas_3],
+    precio: 35000,
+  },
+  {
+    id: 7,
+    nombre: "Zapatillas_Puma_Mayze",
+    img: [puma_1],
+    precio: 35000,
+  },
+  {
+    id: 8,
+    nombre: "Zapatillas_Puma_Mirage",
+    img: [puma_2],
+    precio: 15000,
+  },
+  {
+    id: 9,
+    nombre: "Zapatillas_Puma_RS",
+    img: [puma_3],
+    precio: 25000,
+  },
+];
 
 const getFetch = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(productos)
-        reject(err => console.log(err))
-    }, 3000)
-})
+  setTimeout(() => {
+    resolve(productos);
+    reject((err) => console.log(err));
+  }, 3000);
+});
 
-export default getFetch
-
-
+export default getFetch;
