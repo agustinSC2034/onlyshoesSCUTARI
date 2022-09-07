96885cfed7be21c95ec4f80b28a1d042df774e69
unknown <aguustinn18@gmail.com>
2022-09-07 01:22:17 -0300

    actualizacion final

--- src/components/baseDatos/helper.js
+++ src/components/baseDatos/helper.js
@@ -1,12 +1,21 @@
 import nike_1 from "../../img/Nike/Zapatillas_Nike_Air_Force_1LV8.jpg";
 import nike_2 from "../../img/Nike/Zapatillas_Nike_Air_Max_Dawn.jpg";
 import nike_3 from "../../img/Nike/Zapatillas_Nike_Air_Max_Dia.jpg";
+import nike_4 from "../../img/Nike/Zapatillas_Nike_Court_Vision_Low.jpg";
+import nike_5 from "../../img/Nike/Zapatillas_Nike_Dunk_Low_Se.jpg";
+import nike_6 from "../../img/Nike/Zapatillas_Nike_Tanjun.jpg";
 import adidas_1 from "../../img/Adidas/Zapatillas_adidas_Continental_80.jpg";
 import adidas_2 from "../../img/Adidas/Zapatillas_adidas_Forum_Mid.jpg";
 import adidas_3 from "../../img/Adidas/Zapatillas_adidas_Ozelia.jpg";
+import adidas_4 from "../../img/Adidas/Zapatillas_adidas_Pulse.jpg";
+import adidas_5 from "../../img/Adidas/Zapatillas_adidas_Stan_Smith.jpg";
+import adidas_6 from "../../img/Adidas/Zapatillas_adidas_Zx_2K_Boost_max.jpg";
 import puma_1 from "../../img/Puma/Zapatillas_Puma_Mayze.jpg";
 import puma_2 from "../../img/Puma/Zapatillas_Puma_Mirage.jpg";
 import puma_3 from "../../img/Puma/Zapatillas_Puma_RS.jpg";
+import puma_4 from "../../img/Puma/Zapatillas_Puma_Ferrari_Race_Future.jpg";
+import puma_5 from "../../img/Puma/Zapatillas_Puma_R78_Trek.jpg";
+import puma_6 from "../../img/Puma/Zapatillas_Puma_Rs-X.jpg";
 
 export const arregloProductos = [
   {
@@ -34,22 +43,64 @@ export const arregloProductos = [
   },
   {
     id: 4,
+    nombre: "Zapatillas_Nike_Court_Vision_Low",
+    img: nike_4,
+    precio: '30000',
+    marca: 'Nike',
+  },
+  {
+    id: 5,
+    nombre: "Zapatillas_Nike_Tanjun",
+    img: nike_5,
+    precio: '30000',
+    marca: 'Nike',
+  },
+  {
+    id: 6,
+    nombre: "Zapatillas_Nike_Air_Max_Dia",
+    img: nike_6,
+    precio: '30000',
+    marca: 'Nike',
+  },
+  {
+    id: 7,
     nombre: "adidas_Continental_80",
     img: adidas_1,
     precio: '30000',
     marca: 'Adidas',
   },
   {
-    id: 5,
+    id: 8,
     nombre: "Zapatillas_adidas_Forum_Mid",
     img: adidas_2,
     precio: '30000',
     marca: 'Adidas',
   },
   {
-    id: 6,
-    nombre: "Zapatillas_adidas_Ozelia",
+    id: 9,
+    nombre: "Zapatillas_adidas_Pulse",
     img: adidas_3,
+    precio: '30000',
+    marca: 'Adidas',
+  },
+  {
+    id: 10,
+    nombre: "Zapatillas_adidas_Ozelia",
+    img: adidas_4,
+    precio: '30000',
+    marca: 'Adidas',
+  },
+  {
+    id: 11,
+    nombre: "Zapatillas_adidas_Stan_Smith",
+    img: adidas_5,
+    precio: '30000',
+    marca: 'Adidas',
+  },
+  {
+    id: 12,
+    nombre: "Zapatillas_adidas_Zx_2K_Boost_max",
+    img: adidas_6,
     precio: '30000',
     marca: 'Adidas',
   },
@@ -71,6 +122,27 @@ export const arregloProductos = [
     id: 9,
     nombre: "Zapatillas_Puma_RS",
     img: puma_3,
+    precio: '30000',
+    marca: 'Puma',
+  },
+  {
+    id: 10,
+    nombre: "Zapatillas_Puma_Ferrari_Race_Future",
+    img: puma_4,
+    precio: '30000',
+    marca: 'Puma',
+  },
+  {
+    id: 11,
+    nombre: "Zapatillas_Puma_R78_Trek",
+    img: puma_5,
+    precio: '30000',
+    marca: 'Puma',
+  },
+  {
+    id: 12,
+    nombre: "Zapatillas_Puma_Rs",
+    img: puma_6,
     precio: '30000',
     marca: 'Puma',
   },
